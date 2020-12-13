6cd27181ea304f5b6c71b9140cc74e58dd8fd0b0
Christophe Lambin <christophe.lambin@gmail.com>
2020-12-14 00:51:29 +0100

    refactor packages & unit tests

--- internal/covid/apiclient/mock/mockapi.go
+++ internal/covid/apiclient/mock/mockapi.go
@@ -14,7 +14,7 @@ func New(data map[string]apiclient.CountryStats) *API {
 	return &API{data: data}
 }
 
-// List all data
+// GetCountryStats returns the provided data
 func (api *API) GetCountryStats() (map[string]apiclient.CountryStats, error) {
 	return api.data, nil
 }
--- internal/population/apiclient/mock/mockapi.go
+++ internal/population/apiclient/mock/mockapi.go
@@ -10,7 +10,7 @@ func New(data map[string]int64) *API {
 	return &API{data: data}
 }
 
-// List all data
+// GetPopulation returns the provided data
 func (api *API) GetPopulation() (map[string]int64, error) {
 	return api.data, nil
 }
