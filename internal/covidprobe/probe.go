0549d2a628db25b20accdbf3f3fc0ccc93d53270
Christophe Lambin <christophe.lambin@gmail.com>
2021-01-31 23:47:05 +0100

    merge api & mon servers

--- internal/covidprobe/probe.go
+++ internal/covidprobe/probe.go
@@ -1,26 +1,44 @@
 package covidprobe
 
 import (
+	"covid19/internal/configuration"
 	"covid19/internal/coviddb"
-	"covid19/internal/reporters"
-
+	"fmt"
+	"github.com/clambin/gotools/metrics"
+	"github.com/containrrr/shoutrrr"
+	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 // Probe handle
 type Probe struct {
-	APIClient APIClient
-	db        coviddb.DB
-	reporters *reporters.Reporters
+	APIClient     APIClient
+	db            coviddb.DB
+	notifications *configuration.NotificationsConfiguration
+	lastUpdate    map[string]time.Time
 }
 
 // NewProbe creates a new Probe handle
-func NewProbe(apiKey string, db coviddb.DB, reporters *reporters.Reporters) *Probe {
-	return &Probe{
-		APIClient: NewAPIClient(apiKey),
-		db:        db,
-		reporters: reporters,
+func NewProbe(cfg *configuration.MonitorConfiguration, db coviddb.DB) *Probe {
+	probe := Probe{
+		APIClient:     NewAPIClient(cfg.RapidAPIKey),
+		db:            db,
+		notifications: &cfg.Notifications,
 	}
+
+	if cfg.Notifications.Enabled {
+		// if cfg.Notifications.URL == "" {
+		// 	log.Warning("notifications enabled but no URL set. Ignoring")
+		// } else
+		if len(cfg.Notifications.Countries) == 0 {
+			log.Warning("notifications enabled but not countries specified. Ignoring")
+		} else {
+			probe.lastUpdate = make(map[string]time.Time)
+		}
+	}
+
+	return &probe
 }
 
 // Run gets latest data, inserts any new entries in the DB and reports to Prometheus' pushGateway
@@ -28,19 +46,29 @@ func (probe *Probe) Run() error {
 	var (
 		err          error
 		countryStats map[string]CountryStats
-		newRecords   []coviddb.CountryEntry
+		newRecords   = make([]coviddb.CountryEntry, 0)
 	)
 
-	if countryStats, err = probe.APIClient.GetCountryStats(); err == nil && len(countryStats) > 0 {
+	countryStats, err = probe.APIClient.GetCountryStats()
+
+	if err == nil && len(countryStats) > 0 {
 		log.Debugf("Got %d new entries", len(countryStats))
 
-		if newRecords, err = probe.findNewCountryStats(countryStats); err == nil && len(newRecords) > 0 {
-			if probe.reporters != nil {
-				probe.reporters.Report(newRecords)
-			}
+		newRecords, err = probe.findNewCountryStats(countryStats)
+	}
 
-			log.Infof("Adding %d new entries", len(newRecords))
-			err = probe.db.Add(newRecords)
+	if err == nil && len(newRecords) > 0 {
+		log.Infof("Adding %d new entries", len(newRecords))
+
+		if err = probe.cacheLatestUpdates(); err != nil {
+			log.WithField("err", err).Warning("failed to get latest entries in DB")
+		}
+
+		if err = probe.db.Add(newRecords); err != nil {
+			log.WithField("err", err).Fatal("failed to add new entries in the DB")
+		} else {
+			probe.metricsLatestUpdates(newRecords)
+			err = probe.notifyLatestUpdates(newRecords)
 		}
 	}
 
@@ -59,7 +87,7 @@ func (probe *Probe) findNewCountryStats(newCountryStats map[string]CountryStats)
 			if ok == false || stats.LastUpdate.After(latestUpdate) {
 				code, ok := countryCodes[country]
 				if ok == false {
-					log.Warningf("unknown country '%s'. Skipping", country)
+					log.WithField("country", country).Warning("skipping unknown country")
 				} else {
 					result = append(result, coviddb.CountryEntry{
 						Timestamp: stats.LastUpdate,
@@ -74,4 +102,80 @@ func (probe *Probe) findNewCountryStats(newCountryStats map[string]CountryStats)
 	}
 
 	return result, nil
+}
+
+// cacheLatestUpdates gets the last time for all countries we need to report on
+func (probe *Probe) cacheLatestUpdates() error {
+	var (
+		err           error
+		latestUpdates map[string]time.Time
+	)
+	if probe.lastUpdate != nil {
+		latestUpdates, err = probe.db.ListLatestByCountry()
+
+		if err == nil {
+			for _, country := range probe.notifications.Countries {
+				if lastUpdate, ok := latestUpdates[country]; ok {
+					probe.lastUpdate[country] = lastUpdate
+				}
+			}
+		}
+	}
+	return err
+}
+
+// notifyLatestUpdates sends a notification for each country that has a new update
+func (probe *Probe) notifyLatestUpdates(newEntries []coviddb.CountryEntry) error {
+	var err error
+
+	if probe.lastUpdate != nil {
+		for _, newEntry := range newEntries {
+			// Report to Prometheus
+			// Do we need to send a notification?
+			oldTime, ok := probe.lastUpdate[newEntry.Name]
+
+			if ok == false || newEntry.Timestamp.After(oldTime) {
+				// send notification
+				// FIXME: how to use shoutrrr during unit testing?
+				_ = shoutrrr.Send(probe.notifications.URL,
+					fmt.Sprintf("New covid data for %s\nNew confirmed: %d\nNew deaths: %d\nNew recovered: %d",
+						newEntry.Name,
+						newEntry.Confirmed,
+						newEntry.Deaths,
+						newEntry.Recovered,
+					),
+				)
+
+				probe.lastUpdate[newEntry.Name] = newEntry.Timestamp
+			}
+		}
+	}
+	return err
+}
+
+// Metrics to be reported
+var (
+	reportedCount = metrics.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "covid_reported_count",
+			Help: "New entries per country",
+		},
+		[]string{
+			"country",
+		},
+	)
+)
+
+func (probe *Probe) metricsLatestUpdates(newEntries []coviddb.CountryEntry) {
+	summary := make(map[string]int)
+	for _, newEntry := range newEntries {
+		if count, ok := summary[newEntry.Name]; ok == false {
+			summary[newEntry.Name] = 1
+		} else {
+			summary[newEntry.Name] = count + 1
+		}
+	}
+	for country, count := range summary {
+		reportedCount.WithLabelValues(country).Set(float64(count))
+	}
 }
