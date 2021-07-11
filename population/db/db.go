dc24e61f7bbdd23d3f07ddce307ccd347c0b7744
clambin <christophe.lambin@gmail.com>
2021-07-11 14:19:26 +0200

    populationdb test coverage

--- population/db/db.go
+++ population/db/db.go
@@ -29,19 +29,15 @@ func NewPostgresDB(host string, port int, database string, user string, password
 }
 
 // List all records from the population table
-func (db *PostgresDB) List() (map[string]int64, error) {
-	var (
-		err     error
-		dbh     *sql.DB
-		rows    *sql.Rows
-		entries = make(map[string]int64, 0)
-	)
-
+func (db *PostgresDB) List() (entries map[string]int64, err error) {
 	if err = db.initializeDB(); err == nil {
+		var dbh *sql.DB
 		if dbh, err = sql.Open("postgres", db.psqlInfo); err == nil {
+			var rows *sql.Rows
 			rows, err = dbh.Query(fmt.Sprintf("SELECT country_code, population FROM population"))
 
 			if err == nil {
+				entries = make(map[string]int64, 0)
 				for rows.Next() {
 					var code string
 					var population int64
@@ -49,7 +45,6 @@ func (db *PostgresDB) List() (map[string]int64, error) {
 						entries[code] = population
 					}
 				}
-				log.Debugf("Found %d records", len(entries))
 				_ = rows.Close()
 			}
 			_ = dbh.Close()
@@ -60,34 +55,28 @@ func (db *PostgresDB) List() (map[string]int64, error) {
 
 // Add to population database table. If a record for the specified country code already exists, it will be updated
 func (db *PostgresDB) Add(code string, population int64) (err error) {
-	err = db.initializeDB()
+	if err = db.initializeDB(); err == nil {
+		var dbh *sql.DB
+		dbh, err = sql.Open("postgres", db.psqlInfo)
 
-	if err != nil {
-		return
+		if err == nil {
+			// Prepare the SQL statement
+			sqlStr := fmt.Sprintf("INSERT INTO population(country_code, population) VALUES ('%s', %d) "+
+				"ON CONFLICT (country_code) DO UPDATE SET population = EXCLUDED.population",
+				code, population)
+
+			var stmt *sql.Stmt
+			stmt, err = dbh.Prepare(sqlStr)
+			if err == nil {
+				_, err = stmt.Exec()
+			}
+			if err != nil {
+				log.WithError(err).WithField("sql", sqlStr).Error("failed to insert")
+			}
+			_ = dbh.Close()
+		}
 	}
-
-	var dbh *sql.DB
-	dbh, err = sql.Open("postgres", db.psqlInfo)
-
-	if err != nil {
-		return
-	}
-
-	// Prepare the SQL statement
-	sqlStr := fmt.Sprintf("INSERT INTO population(country_code, population) VALUES ('%s', %d) "+
-		"ON CONFLICT (country_code) DO UPDATE SET population = EXCLUDED.population",
-		code, population)
-
-	var stmt *sql.Stmt
-	stmt, err = dbh.Prepare(sqlStr)
-	if err == nil {
-		_, err = stmt.Exec()
-	} else {
-		log.WithError(err).WithField("sql", sqlStr).Error("failed to insert")
-	}
-	_ = dbh.Close()
-
-	return err
+	return
 }
 
 // initializeDB creates the required tables
@@ -99,19 +88,16 @@ func (db *PostgresDB) initializeDB() (err error) {
 	var dbh *sql.DB
 	dbh, err = sql.Open("postgres", db.psqlInfo)
 
-	if err != nil {
-		return
-	}
-
-	_, err = dbh.Exec(`
+	if err == nil {
+		_, err = dbh.Exec(`
 		CREATE TABLE IF NOT EXISTS population (
 			country_code TEXT PRIMARY KEY,
 			population NUMERIC
 		)
 	`)
 
-	db.initialized = err == nil
-	_ = dbh.Close()
-
-	return err
+		db.initialized = err == nil
+		_ = dbh.Close()
+	}
+	return
 }
